6a8ca7a11c8abf61231ba68397c9ff792b5ce9a9
SkettiSouls <skettisouls@gmail.com>
2024-03-20 13:55:56 -0500

    Parted flake.nix
    Initial commit for the moses branch (flake-parts)

--- flake.nix
+++ flake.nix
@@ -4,47 +4,47 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     hm-unstable.url = "github:nix-community/home-manager/master";
-    hyprland-flake.url = "github:hyprwm/Hyprland";
+    hyprland.url = "github:hyprwm/hyprland";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs:
-    with inputs;
-    let
-      system = "x86_64-linux";
+  outputs = inputs @ { nixpkgs, home-manager, self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
 
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
+      flake = {
+        nixosConfigurations = {
+          goatware = nixpkgs.lib.nixosSystem rec {
+            specialArgs = { inherit inputs self; };
+            modules = [
+              ./configuration.nix
+              home-manager.nixosModules.home-manager
 
-      specialArgs = { inherit inputs self; };
-      moduleArgs = specialArgs // { nixpkgs = pkgs; };
-    in
-    {
-      inherit (import ./packages moduleArgs) packages;
-      nixosConfigurations = {
-        goatware = nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          modules = [
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.skettisouls = import ./home.nix;
+                home-manager.extraSpecialArgs = specialArgs;
+              }
+            ];
+          };
+        };
 
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.skettisouls = import ./home.nix;
-              home-manager.extraSpecialArgs = specialArgs;
-            }
-          ];
+        homeConfigurations = {
+          "skettisouls@goatware" = home-manager.lib.homeManagerConfiguration {
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+            modules = [
+              ./home.nix
+            ];
+          };
         };
       };
 
-      homeConfigurations = {
-        "skettisouls@goatware" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [
-            ./home.nix
-          ];
+      perSystem = { config, lib, pkgs, ... }: {
+        packages = {
+          play = pkgs.callPackage ./packages/play { };
         };
       };
     };
