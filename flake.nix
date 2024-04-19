7b6dc21b19a55c53c94abc6a719dcaaa365a5be1
SkettiSouls <skettisouls@gmail.com>
2024-04-18 20:05:27 -0500

    Added lynx and luni-net.

--- flake.nix
+++ flake.nix
@@ -4,12 +4,25 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     hm-unstable.url = "github:nix-community/home-manager/master";
-    hyprland.url = "github:hyprwm/hyprland";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    lynx.url = "github:the-computer-club/lynx";
+    asluni.url = "github:the-computer-club/automous-zones";
   };
 
-  outputs = inputs @ { nixpkgs, home-manager, self, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ { self, nixpkgs, home-manager, flake-parts, lynx, asluni, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; }
+    {
+      imports = [
+        lynx.flakeModules.flake-guard
+        asluni.flakeModules.asluni
+      ];
+
+      wireguard.networks.asluni.peers.by-name.goatware = {
+        privateKeyFile = /var/lib/wireguard/privatekey;
+        publicKey = "...";
+        ipv4 = [ "172.16.2.7" ];
+      };
+
       systems = [
         "x86_64-linux"
       ];
@@ -21,6 +34,17 @@
             modules = [
               ./configuration.nix
               home-manager.nixosModules.home-manager
+              {
+                imports = [self.nixosModules.flake-guard-host];
+                networking.wireguard = {
+                  networks.asluni.autoConfig = {
+                    interface = true;
+                    peers = true;
+                  };
+
+                  interfaces.asluni.generatePrivateKeyFile = true;
+                };
+              }
 
               {
                 home-manager.useGlobalPkgs = true;
