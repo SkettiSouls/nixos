d1a8e8da68bf9a073bc5e7fd93159eadf8f7b480
SkettiSouls <skettisouls@gmail.com>
2024-07-22 00:43:45 -0500

    Switch to git submodule for scripts
    Removed `flake-sharts/packages/scripts` in favor of a git submodule of `https://github.com/skettisouls/bin`.
    Separated overlays for scripts (pkgs.bin) and packages from self (pkgs.sketti).
    Remove package bundles (to be replaced with meta packages).

--- flake-sharts/packages/default.nix
+++ flake-sharts/packages/default.nix
@@ -1,20 +1,8 @@
 {
   perSystem = { pkgs, ... }: {
     packages = {
-      ### BUNDLES ###
-      aliaspp = pkgs.callPackage ./aliaspp/default.nix {};
-      scripts = pkgs.callPackage ./scripts/default.nix {};
-
-      ### ALIAS++ ###
+      # TODO: Make an alias++ meta package somehow
       rebuild = pkgs.callPackage ./aliaspp/rebuild.nix {};
-
-      ### SCRIPTS ###
-      # TODO: Make backup script
-      connect-headphones = pkgs.callPackage ./scripts/connect-headphones.nix {};
-      eat = pkgs.callPackage ./scripts/eat.nix {};
-      m3u = pkgs.callPackage ./scripts/m3u.nix {};
-      mfs = pkgs.callPackage ./scripts/mfs.nix {};
-      play = pkgs.callPackage ./scripts/play.nix {};
     };
   };
 }
--- flake-sharts/packages/scripts/bash/backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rsync -vrl ~/bin ~/Documents ~/Music ~/Pictures ~/Desktop ~/Videos ~/Vesktop ~/Projects ~/Downloads ~/.wine ~/.config ~/.bashrc /run/media/skettisouls/backup/
-mkdir -pv /run/media/skettisouls/backup/.local/share
-rsync -vrl ~/.local/share/applications ~/.local/share/fonts ~/.local/share/lutris /run/media/skettisouls/backup/.local/share
