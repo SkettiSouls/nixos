45162f4e4152b855cc9e4492462706dbd8a4191d
SkettiSouls <skettisouls@gmail.com>
2024-11-02 00:58:09 -0500

    Apparently fix networking???
    Needed to set a static ip to fix something and then unset the static ip to fix networking,
    even though fluorine is fine???

--- flake-sharts/machines/hosts/argon.nix
+++ flake-sharts/machines/hosts/argon.nix
@@ -13,10 +13,6 @@
   networking = {
     hostName = "argon";
     networkmanager.enable = true;
-    interfaces.enp37s0.ipv4.addresses = [{
-      address = "192.168.1.18";
-      prefixLength = 24;
-    }];
   };
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
