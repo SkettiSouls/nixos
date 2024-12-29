ca0451911a81ac439887b8d17b80a3ee48348886
SkettiSouls <skettisouls@gmail.com>
2024-12-29 07:28:02 -0600

    Fix wireplumber config
    Fixed bluetooth headphones switching to hfp.
    Minor cleanup of pipewire nixos module.

--- src/nixos/modules/pipewire.nix
+++ src/nixos/modules/pipewire.nix
@@ -13,19 +13,18 @@ in
   };
 
   config = mkIf cfg.enable {
+    # Enable realtime priority
     security.rtkit.enable = true;
 
     services.pipewire = {
       enable = true;
       audio.enable = true;
       wireplumber.enable = true;
-      pulse.enable = true;
-      jack.enable = true;
 
-      alsa = {
-        enable = true;
-        support32Bit = true;
-      };
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      jack.enable = true;
+      pulse.enable = true;
     };
   };
 }
--- src/users/skettisouls/home-manager/modules/audio/bluetooth.nix
+++ src/users/skettisouls/home-manager/modules/audio/bluetooth.nix
@@ -17,37 +17,19 @@ in
   config = mkIf cfg.enable {
     # FIXME: Fails on first login after reboot.
     programs.bash.profileExtra = "chp ${headphones.default}"; # Connect headphones automatically on login/ ensure connection on rebuild.
+
     services.mpris-proxy.enable = true;
 
-    home.file = {
-      # Fever dream, not sure if I should remove it.
-      ".config/wireplumber/policy.lua.d/11-bluetooth-policy.lua".text = ''
-        -- Disable bluetooth codec switching
-        bluetooth_policy.policy["media-role.use-headset-profile"] = false
-      '';
-
-      # SHOULD disable PipeWire's HSP/HFP profile.
-      ".config/wireplumber/wireplumber.conf.d/50-bluez.conf".text = ''
-        monitor.bluez.rules = [
-          {
-            matches = [
-              {
-                ## This matches all bluetooth devices.
-                device.name = "~bluez_card.*"
-              }
-            ]
-            actions = {
-              update-props = {
-                bluez5.auto-connect = [ a2dp_sink ]
-                bluez5.hw-volume = [ a2dp_sink ]
-              }
-            }
-          }
-        ]
+    xdg.configFile = {
+      "wireplumber/wireplumber.conf.d/51-disable-hsp-codec.conf".text = ''
+        wireplumber.settings = {
+          ## Disable automatic switching
+          bluetooth.autoswitch-to-headset-profile = false
+        }
 
         monitor.bluez.properties = {
-          bluez.roles = [ a2dp_sink ]
-          bluez.hfphsp-backend = "none"
+          ## Only enable a2dp (highest quality, no mic support)
+          bluez5.roles = [ a2dp_sink a2dp_source ]
         }
       '';
     };
