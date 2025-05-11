7d12c629352160691ef9262a316dc593dffcea87
SkettiSouls <skettisouls@gmail.com>
2025-05-11 11:47:22 -0500

    Minor Hyprland ricing
    Changed `appMod` to modify `mainMod`.
    Added `altMod` (alternative mod not mod_ALT).
    Switched active window border to be purple (color taken from argon gas discharge tube).
    Set up wallpapers and switched from my normal wallpaer (suncat).
    Re-enabled animations.
    Changed animation timings and styles.
    Set new windows to open at the top of the stack (follows river).
    Removed the "App not responding" dialog (why tf was this added????).
    Enabled `initial_workspace_tracking` to prevent discord from opening on the wrong workspace.
    Added binds for special workspace, fixing mfact, and rolling windows.

--- src/machines/argon/skettisouls/desktops/hyprland.nix
+++ src/machines/argon/skettisouls/desktops/hyprland.nix
@@ -4,11 +4,14 @@ let
   inherit (lib) mkIf;
 
   mainMod = "Super";
-  appMod = "Super_SHIFT";
+  appMod = "${mainMod}_SHIFT";
+  altMod = "${mainMod}_CTRL"; # Alternative mod, not `mod_ALT`
+
   defaultHeadphones = config.basalt.headphones.default;
   defaultBrowser = "firefox";
 
   cfg = config.basalt.desktops.hyprland;
+  hcfg = config.wayland.windowManager.hyprland.settings;
 
   bind = mod: key: action: lib.concatStringsSep ", " [ mod key action ];
   bindWorkspaces = mod: action: map (i: bind mod (toString i) "${action}, ${toString i}") (lib.range 1 9);
@@ -28,57 +31,89 @@ let
     (attrValues (concatAttrs rawAttrs));
 in
 {
-  config.wayland.windowManager.hyprland = mkIf cfg.enable {
-    systemd.enable = false; # Conflicts with `withUWSM` option.
-    settings = {
-      monitor = "HDMI-A-1, 1920x1080@60, 0x0, 1";
-      workspace = [
-        "m[HDMI-A-1], layoutopt:orientation:left"
-      ] ++ (map (i: "${toString i}, monitor:HDMI-A-1") (lib.range 1 9));
+  config = mkIf cfg.enable {
+    services.hyprpaper = {
+      enable = true;
+      settings = rec {
+        ipc = "on";
+        splash = false;
 
-      general = {
-        layout = "master";
+        preload = [ "/etc/nixos/etc/images/wallpapers/urple.jpg" ];
+        wallpaper = [ "HDMI-A-1, ${builtins.head preload}" ];
       };
+    };
 
-      animations = {
-        enabled = false;
-      };
+    wayland.windowManager.hyprland = {
+      systemd.enable = false; # Conflicts with `withUWSM` option.
+      settings = {
+        monitor = "HDMI-A-1, 1920x1080@60, 0x0, 1";
+        workspace = [
+          "m[HDMI-A-1], layoutopt:orientation:left"
+        ] ++ (map (i: "${toString i}, monitor:HDMI-A-1") (lib.range 1 9));
 
-      cursor.no_warps = true;
-
-      master = {
-        mfact = 0.6; # Master window size %, 0.6 for river
-        new_status = "master";
-      };
-
-      bind = [
-        ", Print, exec, grime copy area"
-        "${mainMod}_ALT, E, exec, uwsm stop"
-        "${mainMod}, Q, killactive"
-        "${mainMod}, F, fullscreen"
-        "${mainMod}, S, togglefloating"
-        "${mainMod}, RETURN, layoutmsg, swapwithmaster master"
-        "${mainMod}, J, layoutmsg, cyclenext"
-        "${mainMod}, K, layoutmsg, cycleprev"
-      ] ++ 
-      (bindExec {
-        ${mainMod} = {
-          B = "chp ${defaultHeadphones}";
-          R = "fuzzel --launch-prefix=\"uwsm app -- \"";
+        general = {
+          layout = "master";
+          border_size = 2;
+          gaps_in = 6;
+          gaps_out = 12;
+          "col.active_border" = "rgba(9522f0d8)";
+          # "col.inactive_border" = "rgba(9522f060)";
         };
 
-        "${appMod}" = {
-          RETURN = "kitty";
-          B = defaultBrowser;
-          D = "discordcanary";
+        animation = [
+          "windowsIn, 1, 4, default, gnomed"
+          "windowsOut, 1, 5, default, gnomed"
+          "windowsMove, 1, 3, default"
+          "workspaces, 1, 7, default, slidefade"
+          "specialWorkspace, 1, 7, default, fade"
+        ];
+
+        cursor.no_warps = true;
+
+        master = {
+          mfact = 0.6; # Master window size %, 0.6 for river
+          new_status = "master";
+          new_on_top = true;
         };
 
-        "${mainMod}_ALT" = {
-          B = "bluetoothctl disconnect ${defaultHeadphones}";
+        misc = {
+          initial_workspace_tracking = 2;
+          enable_anr_dialog = false;
         };
-      }) ++
-      bindWorkspaces mainMod "workspace" ++
-      bindWorkspaces appMod "movetoworkspacesilent";
+
+        bind = [
+          ", Print, exec, grime copy area"
+          "${altMod}, E, exec, uwsm stop"
+          "${mainMod}, Q, killactive"
+          "${mainMod}, F, fullscreen"
+          "${mainMod}, S, togglefloating"
+          "${mainMod}, Tab, togglespecialworkspace"
+          "${mainMod}, RETURN, layoutmsg, swapwithmaster master"
+          "${mainMod}, J, layoutmsg, cyclenext"
+          "${mainMod}, K, layoutmsg, cycleprev"
+          "${appMod}, J, layoutmsg, rollnext"
+          "${appMod}, K, layoutmsg, rollprev"
+          "${altMod}, R, layoutmsg, mfact exact ${toString hcfg.master.mfact}" # Restore mfact
+        ] ++ 
+        (bindExec {
+          ${mainMod} = {
+            B = "chp ${defaultHeadphones}";
+            R = "fuzzel --launch-prefix=\"uwsm app -- \"";
+          };
+
+          "${appMod}" = {
+            RETURN = "kitty";
+            B = defaultBrowser;
+            D = "discordcanary";
+          };
+
+          "${altMod}" = {
+            B = "bluetoothctl disconnect ${defaultHeadphones}";
+          };
+        }) ++
+        bindWorkspaces mainMod "workspace" ++
+        bindWorkspaces appMod "movetoworkspacesilent";
+      };
     };
   };
 }
