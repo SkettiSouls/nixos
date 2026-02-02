18907c0b798d1257d4602c3d2b6410b4ccfa902d
SkettiSouls <skettisouls@gmail.com>
2026-02-02 10:55:32 -0600

    Minor gaming changes
    Added vintage story and datefudge to packages.
    Enabled ntsync.
    Added all default steam options to `regolith.steam`.`

--- src/machines/argon/skettisouls/gaming.nix
+++ src/machines/argon/skettisouls/gaming.nix
@@ -5,8 +5,8 @@
   config = {
     home.packages = with pkgs; [
       bolt-launcher
-      # Fails to build
-      # inputs.nixpkgs-openmw.legacyPackages.${pkgs.system}.openmw
+      datefudge
+      unstable.vintagestory
     ];
   };
 }
