6e19d580fd09af3cccb535c5860c914bd05c98e1
SkettiSouls <skettisouls@gmail.com>
2026-05-07 01:31:31 -0500

    Refactor pt10: Fix kitty

--- users/skettisouls/wrappers/kitty.nix
+++ users/skettisouls/wrappers/kitty.nix
@@ -16,13 +16,14 @@
             scrollback_lines = 10000;
             background_opacity = 0.8;
             update_check_interval = 0;
+            shell_integration = "no-rc";
 
             ### Tab Bar ###
             tab_bar_edge = "bottom";
             tab_bar_style = "powerline";
             tab_bar_min_tabs = 2;
             tab_switch_strategy = "previous";
-            tabe_fade = "0.25 0.5 0.75 1"; # Play around with this.
+            tab_fade = "0.25 0.5 0.75 1"; # Play around with this.
             tab_powerline_style = "angled";
             tab_title_template = "{index}";
           };
